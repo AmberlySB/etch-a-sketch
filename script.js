d195d38d82ca1614a6d424cd69a946e3db0c1fed
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-07-19 14:37:52 -0500

    Change layout of interface. Implement some input functionality

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-function update() {
-    sliderValue = document.getElementById('myRange').value;
-    const rangeText = document.querySelector('.range-text');
-rangeText.textContent = `Grid size: ${sliderValue} x ${sliderValue}`;
-    console.log(sliderValue);
+//Get user input to determine grid size
+//Add event listener to text box and submit buttons
+
+const userChoice = document.getElementById('user-choice');
+const hintText = document.getElementById('hint');
+const gridText = document.getElementById('grid-text')
+
+userChoice.addEventListener('focus', hint);
+userChoice.addEventListener('keyup', byNumber)
+
+function hint() {
+    hintText.textContent = "Enter a number between 16 and 100";
 }
 
+function byNumber() {
+    let userValue = userChoice.value;
+    gridText.textContent = "x " + userValue;
+}
 
-//const rangeText = document.querySelector('.range-text');
-//rangeText.textContent = `Grid size: ${sliderValue} x ${sliderValue}`;
+/*if (userChoice < 16 || userChoice > 100 || isNaN(userChoice)){
+    hintText.textContent = "ERROR: Enter a number between 16 and 100"
+}*/
+
+//Build grid based on user input
+//this will be a function that uses a loop to iterate through adding the correct amount of divs
