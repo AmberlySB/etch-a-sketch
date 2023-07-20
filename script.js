e2d9b3f00ffac2d35e2392ffaeb6a9943507f4e3
AmberlySB <105951568+AmberlySB@users.noreply.github.com>
2023-07-19 23:41:17 -0500

    Adjust styling and clean up code

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-//Get user input to determine grid size
-//Add event listener to text box and submit buttons
-
+let color = "black";
+let click = false;
 const grid = document.getElementById('grid');
 const userChoice = document.getElementById('user-choice');
 const hintText = document.getElementById('hint');
 const gridText = document.getElementById('grid-text');
 const submitButton = document.getElementById('submit-button');
+const clearButton = document.getElementById('clear');
 
+//This is "toggling" the click variable between true and false
+document.querySelector('#grid').addEventListener('click', () => {click = !click});
 
 
 userChoice.addEventListener('focus', hint);
-userChoice.addEventListener('keyup', byNumber);
+userChoice.addEventListener('keyup', printChoice);
 submitButton.addEventListener('click', makeGrid);
+
+//This simulates a click in order to draw the board on page load
 submitButton.click();
+clearButton.addEventListener('click', clearBoard);
 grid.addEventListener('click', activatePen);
 
 function hint() {
@@ -20,39 +25,34 @@ function hint() {
     hintText.classList.remove('error');
 }
 
-function byNumber() {
+function printChoice() {
     let userValue = userChoice.value;
     gridText.textContent = "x " + userValue;
 }
 
-function validateEntry () {
+function validateUserInput () {
     let userInput = userChoice.value;
     if (userInput < 16 || userInput > 100 || isNaN(userInput)) {
         hintText.textContent = "ERROR: Enter a number between 16 and 100"
         hintText.classList.add('error')
         return false;
-    }
-    else {
+    }else {
         hintText.textContent = "";
         hintText.classList.remove('error');
         return true;
     }
 }
 
-//Build grid based on user input
-//this will be a function that uses a loop to iterate through adding the correct amount of divs
-
 function makeGrid () {
-    if (validateEntry()) {
+    if (validateUserInput()) {
         resetGrid(true);
         let gridSize = userChoice.value;
-        let gridElementSize = 500 / gridSize - 2;
+        let gridElementSize = 500 / gridSize;
         for (i = 0; i < (gridSize * gridSize); i++) {
             const makeDiv = document.createElement('div');
             makeDiv.classList.add('grid-element');
             makeDiv.style.width = `${gridElementSize}px`;
             makeDiv.style.height = `${gridElementSize}px`;
-            //gridSelector.addEventListener('mouseover', colorGrid);
             grid.appendChild(makeDiv);
         }
     }
@@ -74,6 +74,26 @@ function activatePen() {
     }
 }
 
-function colorGrid () {
-    this.classList.add('black');
+function colorGrid() {
+    if (click) {
+            if (color === "Rainbow"){
+            console.log(color);
+            let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            this.style.backgroundColor = "#" + randomColor;
+        }else{
+            console.log(color);
+            this.style.backgroundColor = color;
+        }
+    }
+}
+
+function changeColor (choice) {
+    color = choice;
+}
+
+function clearBoard() {
+    const gridElement = document.querySelectorAll('.grid-element')
+    gridElement.forEach(div => {
+        div.style.backgroundColor = 'lightgray';
+    });
 }
